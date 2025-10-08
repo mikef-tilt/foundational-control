41142078ffd0e8d626e79bfd32e157d331df9e1b
Mike F <mike.fang@tilt.com>
2025-10-08 11:50:31 -0400

    updated with growth summary

--- driver.sh
+++ driver.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-python3 summary_plots.py --input cache/first_loans.parquet --output weekly_loans.png
+python summary_plots.py --input cache/first_loans.parquet --output weekly_loans.png
+python lko.py --input cache/first_loans.parquet --histogram_output weekly_growth_histogram.png
